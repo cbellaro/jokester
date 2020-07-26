10be5d303c956bfa98f81c7a669f9421b8bd65c9
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-07-26 14:07:07 -0400

    added like and dislike animations

--- jokester/src/Components/Buttons.js
+++ jokester/src/Components/Buttons.js
@@ -21,10 +21,10 @@ class Buttons extends React.Component {
     return (
       <div className="button-container">
         <div className={this.state.dislike ? "disliked-joke" : "dislike-button"} onClick={this.handleDislike}>
-          <img src="https://i.imgur.com/MqfzTYo.png"/>
+          <img src={this.state.dislike ? "https://i.imgur.com/YYJgn7x.png": "https://i.imgur.com/MqfzTYo.png"}/>
         </div>
         <div className={this.state.like ? "liked-joke" : "like-button"} onClick={this.handleLike}>
-          <img src="https://i.imgur.com/J6sDQn8.png"/>
+          <img src={this.state.like? "https://i.imgur.com/c14ohBh.png" : "https://i.imgur.com/J6sDQn8.png"}/>
         </div>
       </div>
     )
